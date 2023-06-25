f4a1773c269de0190c1aecaba42ab071b914677f
irfansud2nd <irfansud2nd@gmail.com>
2023-06-26 00:10:57 +0700

    services animation done

--- script.js
+++ script.js
@@ -10,21 +10,62 @@ $("#active").height($("a[href='#home']").height() + 11);
 
 $("a[href^=\\#").on("click", function (event) {
   event.preventDefault();
-  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 1000);
+  $(window).scrollTo($(this.hash), {
+    duration: 1000,
+    onAfter: () => {
+      if (window.scrollY !== $(this.hash).offset().top) {
+        $(window).scrollTo($(this.hash), {
+          duration:
+            (($(this.hash).offset().top - window.scrollY) /
+              $(this.hash).offset().top) *
+            1000,
+        });
+      }
+    },
+  });
   $("#active").animate(
     { left: $(this).position().left - 13.5, width: $(this).width() + 47 },
     { duration: 1000 }
   );
+  if (this.hash == "#services") {
+    $("#services .show .caption").css("animation", "fadeIn 1s .5s forwards");
+  } else {
+    $("#services .show .caption").css("animation", "fadeOut 1s forwards");
+  }
 });
 
-$(".control button").on("click", function (event) {
+$(".control>div").on("click", function (event) {
   let target = event.target.dataset.target;
   if (!$(`#${target}`).hasClass("show")) {
+    $("#services .show .caption").css("animation", "fadeOut 1s forwards");
     $(".show").removeClass("show");
     $(`#${target}`).addClass("show");
     $(`#${target}`).css("top", "100%");
     $(`#${target}`).animate({ top: 0 }, 500);
+    let x = $(".image").filter(function () {
+      return $(this).hasClass("show") == false;
+    });
+    setTimeout(() => {
+      x.css("top", "100%");
+    }, 500);
+    $("#services .show .caption").css("animation", "fadeIn 1s .4s forwards");
+  }
+  if (!$(`#${target}`).hasClass("active-control")) {
+    $(".active-control").removeClass("active-control");
+    $(this).addClass("active-control");
+    console.log(this);
   }
 });
 
-// console.log($("#restore").css("top"));
+function alertsucces(e) {
+  e.preventDefault();
+  swal({
+    title: "Succes!",
+    text: "its just a dummy form :)",
+    icon: "success",
+  }).then(() => {
+    $("form>div>input,form>div>textarea").val("");
+    console.log(window.scrollY, $("#contact").offset().top);
+  });
+  return false;
+}
